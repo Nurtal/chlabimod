24c86f49631734728b6234b8fd86aca227957126
Nurtal <nathan.foulquier.pro@gmail.com>
2026-02-23 15:57:55 +0100

    refactor

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from models import Chlabichou, Cellule, Gene
 
-# Création de gènes
-g1 = Gene("A", 0.8)
-g2 = Gene("B", 0.5)
-g3 = Gene("C", 0.3)
 
-# Création cellule
-cellule = Cellule("neurone", [g1, g2, g3])
+def test_run():
+    """ """
 
-# Création animal
-animal = Chlabichou("Chat", [cellule])
+    # Création de gènes
+    g1 = Gene("A", 0.8)
+    g2 = Gene("B", 0.5)
+    g3 = Gene("C", 0.3)
 
-print(cellule)
-print("Expression globale :", animal.expression_globale())
+    # Création cellule
+    cellule = Cellule("neurone", [g1, g2, g3])
+
+    # Création animal
+    animal = Chlabichou("Chat", [cellule])
+
+    print(cellule)
+    print("Expression globale :", animal.expression_globale())
+
+
+
+if __name__ == "__main__":
+
+    test_run()
